6511a9cb0d7febe327d95d2cb24281d0697a821e
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-28 00:25:10 +0300

    Cange "Switch" to "Routes"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navigation/NavBar';
 import MissionsPage from './components/missions/MissionsPage';
-// import RocketsPage from './components/rockets/RocketsPage';
 import ProfilePage from './components/profile/myProfile';
 import NoMatch from './components/NoMatch/NoMatch';
 import './App.css';
@@ -10,20 +9,11 @@ import './App.css';
 const App = () => (
   <Router basename={process.env.PUBLIC_URL}>
     <NavBar />
-    <Switch>
-      {/* <Route exact path='/'>
-        <RocketsPage />
-      </Route> */}
-      <Route path="/missions">
-        <MissionsPage />
-      </Route>
-      <Route path="/profile">
-        <ProfilePage />
-      </Route>
-      <Route path="*">
-        <NoMatch />
-      </Route>
-    </Switch>
+    <Routes>
+      <Route path="/mission" element={<MissionsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
   </Router>
 );
 
