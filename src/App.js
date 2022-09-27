76508c4e0cbaf38686405e06df421f910bd9b106
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-27 21:11:05 +0300

    Add Router and wrap it inside components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/navigation/NavBar';
+import MissionsPage from './components/missions/MissionsPage';
+// import RocketsPage from './components/rockets/RocketsPage';
+import ProfilePage from './components/profile/myProfile';
+import NoMatch from './components/NoMatch/NoMatch';
 import './App.css';
 
 const App = () => (
-  <>
-    <Router>
-      <NavBar />
-    </Router>
-  </>
+  <Router basename={process.env.PUBLIC_URL}>
+    <NavBar />
+    <Switch>
+      {/* <Route exact path='/'>
+        <RocketsPage />
+      </Route> */}
+      <Route path="/missions">
+        <MissionsPage />
+      </Route>
+      <Route path="/profile">
+        <ProfilePage />
+      </Route>
+      <Route path="*">
+        <NoMatch />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
