70cc45668f8f5ac857eaab740cab54d4470f9b71
Oluyemi Paul <oluola96@gmail,com>
2022-09-27 18:41:24 +0100

    Feat: Add style navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css';
 import Navbar from './components/Navbar';
+import './App.css';
+// import NotFound from './components/pages/NotFound';
+// import Dragons from './components/dragon/dragons';
+// import MissionsContainer from './components/pages/MissionsContainer';
+// import Rockets from './components/rockets/Rockets';
+// import ProfileContainer from './components/pages/ProfileContainer';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <h1>Space Travelers hub</h1>
+    <div className="container">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/space-travelers" exact="true" element={<h1>Rocket Page</h1>} />
+          <Route path="/missions" element={<h1>Mission Page</h1>} />
+          <Route path="/myprofile" element={<h1>Booking Record Page</h1>} />
+          <Route path="*" element={<h1>Not Found 404 Page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,81 @@
-import React from 'react';
-import { NavLink } from "react-router-dom";
+import React, { Component } from 'react';
+import { NavLink, Link } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import image from '../image/RGP-icon.png';
+import { render } from '@testing-library/react';
 
-const Navbar = () => {
+class Navbar extends Component {
+    constructor(){
+        super();
+        this.state = { links: [
+            {
+              id: 1,
+              path: '/space-travelers',
+              text: 'Rockets',
+            },
+            {
+              id: 2,
+              path: '/missions',
+              text: 'Missions',
+            },
+            {
+              id: 3,
+              path: '/dragons',
+              text: 'Dragons',
+            },
+            {
+              id: 4,
+              path: '/myprofile',
+              text: 'My Profile',
+            },
+          ]};
+    }
 
-    const links = [
-        {
-          id: 1,
-          path: '/space-travelers',
-          text: 'Rockets',
-        },
-        {
-          id: 2,
-          path: '/missions',
-          text: 'Missions',
-        },
-        {
-          id: 3,
-          path: '/dragons',
-          text: 'Dragons',
-        },
-        {
-          id: 4,
-          path: '/myprofile',
-          text: 'My Profile',
-        },
-      ];
-
+      render() {
     return (
-        <nav className="nav-bar">
-            <h1>Navbar</h1>
-       <div className="brand-logo-container">
-            <img className="spacelogo" src={image} alt="spacelogo" />
-            <h2 className="nav-brand">Space Travellers&apos; Hub</h2>
-          </div>
-          <ul className="nav-links">
-            {links.map((link) => (
-              <li className="nav-link" key={link.id}>
-                <NavLink to={link.path}>
-                  {link.text}
+      //   <nav className="navbar bg-light">
+      //     <div className="container-fluid">
+      //  <div className="brand-logo-container">
+      //       <img className="d-inline-block align-text-top" src={image} alt="spacelogo" />
+      //       <h2 className="nav-brand">Space Travellers&apos; Hub</h2>
+      //     </div>
+      //     <ul className="nav-links">
+      //       {this.state.links.map((link) => (
+      //        <li className="nav-item" key={link.id}>
+      //            <NavLink  className="nav-link" to={link.path}>
+      //              {link.text}
+      //           </NavLink>
+      //         </li>
+      //       ))}
+    
+      //     </ul> 
+      //     </div>
+      //   </nav>
+      <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+  <Link className="navbar-brand" to="/space-travelers">
+      <img src={image} alt="Logo" width="30" height="30" className="d-inline-block align-text-top" />
+      Space Travellers&apos; Hub
+    </Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+      {this.state.links.map((link) => (
+             <li className="nav-item" key={link.id}>
+                 <NavLink  className="nav-link" to={link.path}>
+                   {link.text}
                 </NavLink>
               </li>
             ))}
-    
-          </ul> 
-        </nav>
+      </ul>
+    </div>
+  </div>
+</nav>
       );
+    }
     };
     export default Navbar;
 
