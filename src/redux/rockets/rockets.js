ce84f8a3ea0f88b9ea9dd383557fd69650bb4974
Oluyemi Paul <oluola96@gmail,com>
2022-09-28 17:14:43 +0100

    Fix: Resolve elint error

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,6 @@
 import APIServices from './APIServices';
 
-//Create Rocket ActionTypes
+// Create Rocket ActionTypes
 export const GET_ROCKET_DATA = 'spacetravelers/rockets/GET_ROCKET_DATA';
 export const ROCKET_RESERVATION = 'spacetravelers/rockets/ROCKET_RESERVATION';
 export const CANCEL_ROCKET_RESERVATION = 'spacetravelers/rockets/CANCEL_ROCKET_RESERVATION';
