64343f15cb042df1a2ccb5608976c10aa96dd4e1
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-29 16:40:40 +0300

    Implement mission resrved logic

--- src/components/pages/ProfileContainer.js
+++ src/components/pages/ProfileContainer.js
@@ -1,9 +1,11 @@
 // import ProfileMissions from '../missions/ProfileMissions';
+import AllJoinedMissions from './JoinedMission';
 import ProfileRocket from '../rockets/ProfileRocket';
 
 const ProfileContainer = () => (
   <div className="profileContainer">
     <ProfileRocket />
+    <AllJoinedMissions />
     {/* <ProfileMissions /> */}
   </div>
 );
